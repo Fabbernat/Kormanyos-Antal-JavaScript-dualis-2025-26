03fb4775807d26fa6b692e8c5fc2ba669df4bacb
Fabbernat <fabbernat@gmail.com>
2025-09-04 16:40:09 +0200

    very improved objectFeladatok

--- objectFeladatok.js
+++ objectFeladatok.js
@@ -1,55 +1,74 @@
 import input from "./input.js";
 
-class Tanulo{
-    constructor(nev, emailCim) {
-        this._nev = nev;
-        this._emailCim = emailCim;
-    }
+class Tanulo {
+  constructor(nev, emailCim) {
+    this._nev = nev;
+    this._emailCim = emailCim;
+  }
 
-    getNev() {
-        return this._nev;
-    }
+  getNev() {
+    return this._nev;
+  }
 
-    getEmailCim() {
-        return this._emailCim;
-    }
+  getEmailCim() {
+    return this._emailCim;
+  }
 
-    setNev(nev) {
-        this._nev = nev;
-    }
+  setNev(nev) {
+    this._nev = nev;
+  }
 
-    setEmailCim(emailCim) {
-        this._emailCim = emailCim;
-    }
-}
+  setEmailCim(emailCim) {
+    this._emailCim = emailCim;
+  }
 
-const tanulok = [];
-const adat = await input("Kérek egy számot: ");
-for (let i = 0; i < Number(adat); i++) {
-    const nev = await input("Kérem a nevet: ");
-    const emailCim = await input("Kérem az email címet: ");
-    const tanulo = new Tanulo(nev, emailCim);
-    tanulok.push(tanulo); // .push instead of .add
+  static toString() {
+    return `${this.getNev()}, ${this.getEmailCim()}`;
+  }
 }
 
 class Orarend {
-    constructor(hetiOrak = []) {
-        this.hetiOrak = hetiOrak;
-    }
+  constructor(
+    hetiOrak = [
+      { hetfo: ["matematika", "angol"] },
+      { kedd: ["Webfejlesztés", "C#"] },
+      { szerda: ["JavaScript", "WordPress"] },
+      { csutortok: ["Fizika", "Biológia"] },
+      { pentek: ["Történelem", "Földrajz"] },
+    ]
+  ) {
+    this._hetiOrak = hetiOrak;
+  }
 
-    getHetiOrak() {
-        return this.hetiOrak;
-    }
+  getHetiOrak() {
+    return this._hetiOrak;
+  }
 
-    setHetiOrak(hetiOrak) {
-        this.hetiOrak = hetiOrak;
-    }
+  setHetiOrak(hetiOrak) {
+    this._hetiOrak = hetiOrak;
+  }
 
-    add(ora) {
-        this.hetiOrak.push(ora);
-    }
+  add(ora) {
+    this._hetiOrak.push(ora);
+  }
 }
+
+const tanulok = [];
+const adat = await input("Hány tanuló adatait szeretné bevinni?");
+for (let i = 0; i < Number(adat); i++) {
+  const nev = await input("Kérem a nevet: ");
+  const emailCim = await input("Kérem az email címet: ");
+  const tanulo = new Tanulo(nev, emailCim);
+  tanulok.push(tanulo);
+}
+
+console.log(tanulok.map((t) => t.toString()));
 
 const orakAsStr = await input("Add meg az órákat vesszővel elválasztva: ");
 const orarend = new Orarend();
 orarend.setHetiOrak(orakAsStr.split(","));
+
+console.log(orarend.getHetiOrak());
+
+orarend._hetiOrak[0].hetfo.push("Fizika");
+console.log(orarend.getHetiOrak());
